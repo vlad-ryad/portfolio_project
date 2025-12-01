07018286ba2f17aa57b032f67079bb29b4e5be15
vlad-ryad <work.ryadinsky@gmail.com>
2025-12-01 12:36:04 +0300

    update info about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1I71dFz6JeAWz-dCgtaWEjZ4JH5yVdIUS/view?usp=sharing"
+            href="https://drive.google.com/file/d/1EVG_TdS6uYdT14yA7m00QKdV1jk0FKAs/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
